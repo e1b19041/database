9c6f5335cddf9eb6896cb2b99fe657c38301fc42
e1b19049 <e1b19049@st.oit.ac.jp>
2021-10-19 15:41:00 +0900

    最初の入室者の取得の実装

--- src/main/java/team11/lec04/database/controller/Sample41Controller.java
+++ src/main/java/team11/lec04/database/controller/Sample41Controller.java
@@ -3,6 +3,12 @@ package team11.lec04.database.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.ModelMap;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import team11.lec04.database.model.Chamber;
+import team11.lec04.database.model.ChamberMapper;
 
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
@@ -11,8 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 }
