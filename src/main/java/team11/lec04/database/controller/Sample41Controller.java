a0815b037f2bc81c8a9868be5af3d136eaacbe95
e1b19057 <e1b19057@oit.ac.jp>
2021-10-19 16:08:33 +0900

    sample44への移動の実装

--- src/main/java/team11/lec04/database/controller/Sample41Controller.java
+++ src/main/java/team11/lec04/database/controller/Sample41Controller.java
@@ -32,6 +32,11 @@ public class Sample41Controller {
     return "sample41.html";
   }
 
+  @GetMapping("step4")
+  public String sample44() {
+    return "sample44.html";
+  }
+
   @GetMapping("step2/{id}")
   public String sample42(@PathVariable Integer id, ModelMap model) {
     Chamber chamber2 = chamberMapper.selectById(id);
